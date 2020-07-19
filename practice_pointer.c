7d296ddbd2bd77ba507f13c7775b8fd6fa2d20f0
shusuke0812 <shu.ota0812@gmail.com>
2020-07-20 05:37:16 +0900

    [add]ポインタ変数を引数に持つ関数を定義

--- practice_pointer.c
+++ practice_pointer.c
@@ -4,9 +4,12 @@
 
 #include <stdio.h>
 
+void func(int *pvalue);
+
 int main(void) {
 	int *p, i;
 	int *q;
+	int value = 10;
 
 	// iのアドレスをポインタ変数pに代入する
 	p = &i;
@@ -15,5 +18,16 @@ int main(void) {
 	printf("p=%p\n", &i);
 	printf("p=%p\n", q);
 
+	// ポインタ変数を引数にする
+	printf("&value= %p\n", &value);
+	func(&value);
+	printf("value= %d\n", value);
+
 	return 0;
+}
+
+void func(int *pvalue) {
+	printf("pvalue= %p\n", pvalue);
+	*pvalue = 100;
+	return;
 }
